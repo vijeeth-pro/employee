c07c9fa20d555a5269cfe30c3a58831cf8f05aa6
vijeeth-grelin <vijeeth.s@grelinhealth.com>
2026-08-18 15:43:26 +0530

    fixing server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,14 +7,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, "")
-      },
-    },
-  },
 });
 
